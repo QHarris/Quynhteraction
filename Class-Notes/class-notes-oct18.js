a778e87a93fb69ba26cfb73b38ec9c78ee0cb27d
QHarris <harris.quynh@gmail.com>
2022-10-25 15:40:08 -0400

    New museum landing

---
+++ Class-Notes/class-notes-oct18.js
@@ -0,0 +1,72 @@
+console.log(100);
+console.log("100");
+console.log("whats up baby");
+console.log(true);
+console.log("true");
+
+let MyVariable;
+MyVariable = 100;
+console.log(MyVariable);
+
+let MyVariable2;
+MyVariable2 = "heyyy";
+console.log(MyVariable2);
+
+let MyVariable3 = "takemeouttodinner, ";
+let MyVariable4 = "baby";
+console.log(MyVariable3 + MyVariable4);
+
+//let MyVariable5 = MyVariable3 + MyVariable4;
+//console.log(MyVariable5V);
+
+let Number1 = 100;
+let Number2 = 200;
+let Number3 = Number1 + Number2;
+console.log(Number3);
+
+let Variable6 = MyVariable3 + Number1;
+console.log(Variable6);
+
+var MyVariableA;
+MyVariableA = 10;
+console.log(MyVariableA);
+var buttonVariable2;
+
+var myvariable;
+var MYVARIABLE = 100;
+
+console.log(myvariable);
+
+console.log(5 ** 2);
+var buttonWidth;
+
+
+$("#button").click(function(){
+    buttonWidth = $(this).width();
+    if (buttonWidth == 1000){
+        buttonWidth = 100;
+        $(this).width(buttonWidth);
+    }else{
+        buttonWidth = 1000;
+        $(this).width(buttonWidth);
+    };
+});
+    
+$("#button2").click(function(){
+    let btn2width = Math.random();
+    console.log(btn2width);
+});
+ 
+//    if(1 == 1){
+//        var variableB = 200;
+//        var variableC = 300;
+//        console.log(variableB);
+//        console.log(variableC);
+//    };
+//    
+//    console.log(variableB);
+//    console.log(variableC);
+
+
+
+
